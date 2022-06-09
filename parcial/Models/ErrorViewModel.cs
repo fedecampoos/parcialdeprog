6b88d42f8e15a726646980f29904dc50267bea7b
fedecampoos <40859084+fedecampoos@users.noreply.github.com>
2022-06-08 22:22:08 -0300

    git solo de fede
    8/6

---
+++ parcial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace parcial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ parcial/Models/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+#nullable disable
+
+namespace parcial.Models
+{
+    public partial class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Producto")]
+        public string NombreProducto { get; set; }
+        public string Categoria { get; set; }
+        public int? Precio { get; set; }
+        public int? Stock { get; set; }
+    }
+}
